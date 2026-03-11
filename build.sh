11e4ae72d971a24cb8903a06f2d5336986fd74cc
matteoepitech <matteo.tutti@epitech.eu>
2026-03-11 15:44:51 +0100

    feat(base): add the base of the project and the CMakeLists.txt

---
+++ build.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+
+rm -rf ./build/
+mkdir -p build
+cd build && cmake ..
+make
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** DEL PROJECT, 2026
+** src/main
+** File description:
+** Main file
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
